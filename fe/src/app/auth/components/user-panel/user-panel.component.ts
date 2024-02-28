7ffc38078a1da1f02fad8d17f3efe38c0b4df6fe
hudagon <dohoangduc1112000@gmail.com>
2024-02-28 07:57:10 +0700

    nopbai

--- fe/src/app/auth/components/user-panel/user-panel.component.ts
+++ fe/src/app/auth/components/user-panel/user-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -7,6 +7,7 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./user-panel.component.css']
 })
 export class UserPanelComponent {
+  @Input() page: string = "";
   
   constructor(
     private authService: AuthService,
